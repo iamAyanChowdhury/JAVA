8d3cdcf7f482c82df59266ac384fad31c9d7f80f
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-10-05 23:15:54 -0700

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AreaOfRectangle
+{
+    public static void main(String[]args)
+    {
+        int length,breadth,area;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length: ");
+        length=sc.nextInt();
+        System.out.println("Enter the breadth: ");
+        breadth=sc.nextInt();
+        area=length*breadth;
+        System.out.println("The area is : " +area);
+
+        
+
+    }
+}

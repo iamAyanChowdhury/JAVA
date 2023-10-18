f5e9d7a007557477ebecb5d70169aa90462ee3f7
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-10-18 08:56:44 +0530

    Create Celsiustofarenheit.java

---
+++ Celsiustofarenheit.java
@@ -0,0 +1,9 @@
+public class Celsiustofarenheit
+ {  
+   public static void main (String args[])  
+    { float Fahrenheit, Celsius;  
+          Fahrenheit = 43;  
+          Celsius  = ((Fahrenheit-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+Celsius);  
+    }
+ }  

3a49c153f4ba59eb253f2cda2c829cde975f5221
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-10-19 19:19:52 +0530

    Create LargestNumber1.java

---
+++ LargestNumber1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;  
+public class LargestNumber1
+{  
+public static void main(String[] args)   
+{  
+int a, b, c, largest, temp;  
+Scanner sc = new Scanner(System.in); 
+System.out.println("Enter the first number:");  
+a = sc.nextInt();  
+System.out.println("Enter the second number:");  
+b = sc.nextInt();  
+System.out.println("Enter the third number:");  
+c = sc.nextInt();  
+//comparing a and b and storing the largest number in a temp variable  
+temp=a>b?a:b;  
+//comparing the temp variable with c and storing the result in the variable  
+largest=c>temp?c:temp;  
+//prints the largest number  
+System.out.println("The largest number is: "+largest);  
+}  
+}  

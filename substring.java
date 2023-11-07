0ef742b400382249300211156d072fd6d5202309
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-11-07 09:23:37 -0800

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class substring 
+{
+    public static void main(String[]args)
+    {
+        String s="My name is Ayan";
+        String name=s.substring(11, s.length());
+        System.out.println(name);
+
+    }
+}

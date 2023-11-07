2cbfb690f7db9da4707855fe988a70b9a2f5e4ea
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-11-07 09:24:43 -0800

    Add files via upload

---
+++ parse_Int.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class parse_Int 
+{
+    public static void main(String[] args) 
+    {
+      String str="123";
+      int number=Integer.parseInt(str);
+      System.out.println(number);    
+    }
+    
+}

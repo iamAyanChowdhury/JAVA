4807080a515a0946fcee81f764a927aea4e68b00
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-11-16 10:57:26 -0800

    Add files via upload

---
+++ twodArray.java
@@ -0,0 +1,16 @@
+public class twodArray {
+    public static void main(String[] args) {
+        int a[][]={{1,2,3,4},{2,4,6,8},{3,5,7,9}};
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a[0].length;j++)
+            {
+                System.out.print(a[i][j]+" ");
+                
+            }
+            System.out.println("");
+        }
+       
+    }
+    
+}

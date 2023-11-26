b723a9b919e7809d07983fc4f45898e6a17852b2
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-11-26 06:27:37 -0800

    recursion basics

---
+++ recursion1.java
@@ -0,0 +1,14 @@
+public class recursion1 {
+   static void fun(int n)
+    {
+        if(n>0)
+        {
+            System.out.println(n);
+            fun(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        fun(3);
+    }
+    
+}
---
+++ recursion2.java
@@ -0,0 +1,14 @@
+public class recursion2 {
+    static void fun(int n)
+    {
+        if(n>0)
+        {
+            fun(n-1);
+            System.out.println(n);
+        }
+    }
+    public static void main(String[] args) {
+        fun(3);
+    }
+    
+}
